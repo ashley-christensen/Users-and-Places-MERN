04c22ff7c6da9d0dcb417abec4cb0f151433ad6e
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2023-06-08 13:14:28 -0700

    more files setup

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,19 @@
 import React from 'react';
+
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'ul',
-            image: "https://dummyimage.com/",
-            name: 'Ashley Christensen',
-            places: 3
-        }
-    ];
-    return <UsersList items={USERS} />
-        ;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Ashley Christensen',
+      image:
+        'https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_1280.jpg',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
